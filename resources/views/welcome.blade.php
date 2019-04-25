198d3200d2d7d3c51c8b633452de6b1501bb78be
cesar clemente fernandez dorantes <c.clemente1@hotmail.com>
2019-04-25 16:03:29 -0500

    Aplicacion de bootstrap 4 y desarrollo de tarjeta en welcome.blade

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,10 +2,21 @@
 
 @section('content')
 
-
-<form action="{{ route('statuses.store') }}" method="POST">
-    @csrf
-    <textarea name="body" cols="30" rows="10"></textarea>
-    <button id="create-status">Publicar estado</button>
-</form>
+    <div class="container">
+        <div class="row">
+            <div class="col-8 mx-auto">
+                <div class="card border-0 bg-light">
+                    <form action="{{ route('statuses.store') }}" method="POST">
+                        @csrf
+                        <div class="card-body">
+                            <textarea class='form-control border-0 bg-light' name="body" placeholder="¿Qué estas pensando?"></textarea>
+                        </div>
+                        <div class="card-footer">
+                            <button class='btn btn-primary' id="create-status">Publicar</button>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
 @endsection

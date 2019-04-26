fcab8d5a92092a7b83d965a857826369c4fe47bc
cesar clemente fernandez dorantes <c.clemente1@hotmail.com>
2019-04-25 22:11:30 -0500

    Desarrollo de factory para statuses, se agrego un ruta en web.php, cambios en StatusesController, creación de prueba de listado de status, implementación de seeds, uso de vue combios en el blade de welcome

---
+++ database/factories/StatusFactory.php
@@ -0,0 +1,16 @@
+<?php
+
+/* @var $factory \Illuminate\Database\Eloquent\Factory */
+
+use App\User;
+use Faker\Generator as Faker;
+
+$factory->define(App\Models\Status::class, function (Faker $faker) {
+    return [
+        'body' => $faker->paragraph,
+        'user_id' => function()
+        {
+            return factory(User::class)->create();
+        }
+    ];
+});

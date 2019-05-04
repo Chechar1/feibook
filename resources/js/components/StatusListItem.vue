cf6902a5a8789b6292cd47d0b0455853573c81ad
cesar clemente fernandez dorantes <c.clemente1@hotmail.com>
2019-05-04 18:29:19 -0500

    Cambios en StatusListItem.vue

--- resources/js/components/StatusListItem.vue
+++ resources/js/components/StatusListItem.vue
@@ -20,6 +20,14 @@
                 <i class="far fa-thumbs-up"></i>
                 <span dusk="likes-count">{{ status.likes_count }}</span>
             </div>
+
+            <form @submit.prevent="addComment">
+                <textarea name="comment" v-model="newComment"></textarea>
+                <button dusk="comment-btn">Enviar</button>
+            </form>
+
+            <div v-for="comment in comments">{{ comment.body }}</div>
+
         </div>
     </div>
 </template>
@@ -34,7 +42,22 @@
                 required: true
             },
         },
-        components: { LikeBtn }
+        components: { LikeBtn },
+        data(){
+            return {
+                newComment: '',
+                comments: []
+            }
+        },
+        methods: {
+            addComment(){
+                axios.post(`/statuses/${this.status.id}/comments`, {body: this.newComment})
+                    .then(res => {
+                        this.newComment = '';
+                        this.comments.push(res.data.data);
+                    })
+            }
+        }
     }
 </script>
 

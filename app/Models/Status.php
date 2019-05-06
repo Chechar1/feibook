8c247388342f0b1326f2b1cd264e3f6309e25fc6
cesar clemente fernandez dorantes <c.clemente1@hotmail.com>
2019-05-05 20:07:12 -0500

    Creacion de trait para likes e implementacion

--- app/Models/Status.php
+++ app/Models/Status.php
@@ -3,10 +3,12 @@
 namespace App\Models;
 
 use App\User;
+use App\Traits\HasLikes;
 use Illuminate\Database\Eloquent\Model;
 
 class Status extends Model
 {
+    use HasLikes;
     protected $guarded = [];
 
     public function user()
@@ -17,34 +19,5 @@ class Status extends Model
     public function comments()
     {
         return $this->hasMany(Comment::class);
-    }
-
-    public function likes()
-    {
-        return $this->morphMany(Like::class, 'likeable');
-    }
-
-    public function like()
-    {
-        $this->likes()->firstOrCreate([
-            'user_id' => auth()->id()
-        ]);
-    }
-
-    public function unlike()
-    {
-        $this->likes()->where([
-            'user_id' => auth()->id()
-        ])->delete();
-    }
-
-    public function isLiked()
-    {
-        return $this->likes()->where('user_id', auth()->id())->exists();
-    }
-
-    public function likesCount()
-    {
-        return $this->likes()->count();
     }
 }
--- app/Traits/hasLikes.php
+++ app/Traits/hasLikes.php
@@ -2,9 +2,36 @@
 
 namespace App\Traits;
 
-use Illuminate\Database\Eloquent\Model;
+use App\Models\Like;
 
-class hasLikes extends Model
+trait HasLikes
 {
-    //
+    public function likes()
+    {
+        return $this->morphMany(Like::class, 'likeable');
+    }
+
+    public function like()
+    {
+        $this->likes()->firstOrCreate([
+            'user_id' => auth()->id()
+        ]);
+    }
+
+    public function unlike()
+    {
+        $this->likes()->where([
+            'user_id' => auth()->id()
+        ])->delete();
+    }
+
+    public function isLiked()
+    {
+        return $this->likes()->where('user_id', auth()->id())->exists();
+    }
+
+    public function likesCount()
+    {
+        return $this->likes()->count();
+    }
 }

08dd044a192cac127a1c6e7f3428cdfd9fa14e3e
cesar clemente fernandez dorantes <c.clemente1@hotmail.com>
2019-05-11 09:43:40 -0500

    Probando el registro con Dusk

--- tests/Feature/RegistrationTest.php
+++ tests/Feature/RegistrationTest.php
@@ -15,6 +15,8 @@ class RegistrationTest extends TestCase
     /** @test */
     public function users_can_register()
     {
+        $this->get(route('register'))->assertSuccessful();
+
         $response = $this->post(route('register'), $this->userValidData());
 
         $response->assertRedirect('/');

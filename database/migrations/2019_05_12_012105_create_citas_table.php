ede76197e96626549001b01e61a65f12fbb8da34
cesar clemente fernandez dorantes <c.clemente1@hotmail.com>
2019-05-11 20:22:49 -0500

    Cambio en la migracion de citas

--- database/migrations/2019_05_12_012105_create_citas_table.php
+++ database/migrations/2019_05_12_012105_create_citas_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateCitaTable extends Migration
+class CreateCitasTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,7 +13,7 @@ class CreateCitaTable extends Migration
      */
     public function up()
     {
-        Schema::create('cita', function (Blueprint $table) {
+        Schema::create('citas', function (Blueprint $table) {
             $table->increments('id');
             $table->unsignedInteger('sender_id')->index();
             $table->unsignedInteger('recipient_id')->index();
@@ -29,6 +29,6 @@ class CreateCitaTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('cita');
+        Schema::dropIfExists('citas');
     }
 }

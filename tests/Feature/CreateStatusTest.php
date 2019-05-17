541c402e8646ce6a306bf2b47c63059398b5c35e
cesar clemente fernandez dorantes <c.clemente1@hotmail.com>
2019-05-17 13:36:16 -0500

    Testear eventos

--- tests/Feature/CreateStatusTest.php
+++ tests/Feature/CreateStatusTest.php
@@ -4,7 +4,10 @@ namespace Tests\Feature;
 
 use App\User;
 use Tests\TestCase;
-use Illuminate\Foundation\Testing\WithFaker;
+use App\Models\Status;
+use App\Events\StatusCreated;
+use Illuminate\Support\Facades\Event;
+use App\Http\Resources\StatusResource;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 
 class CreateStatusTest extends TestCase
@@ -22,10 +25,17 @@ class CreateStatusTest extends TestCase
     /** @test */
     public function an_authenticated_user_can_create_statuses()
     {
+        Event::fake([StatusCreated::class]);
+
         $user = factory(User::class)->create();
         $this->actingAs($user);
 
         $response = $this->postJson(route('statuses.store'), ['body' => 'Mi primer status']);
+
+        Event::assertDispatched(StatusCreated::class, function ($e) {
+            return $e->status->id === Status::first()->id
+                && get_class($e->status) === StatusResource::class;
+        });
 
         $response->assertJson([
             'data' => ['body' => 'Mi primer status'],

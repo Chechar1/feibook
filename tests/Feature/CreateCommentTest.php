103b7ec75f7ea96ade39bd028345f620d042b2b4
cesar clemente fernandez dorantes <c.clemente1@hotmail.com>
2019-05-18 08:44:30 -0500

    Comentarios en tiempo real 25%

--- tests/Feature/CreateCommentTest.php
+++ tests/Feature/CreateCommentTest.php
@@ -5,7 +5,13 @@ namespace Tests\Feature;
 use App\User;
 use Tests\TestCase;
 use App\Models\Status;
+use App\Models\Comment;
+use App\Events\CommentCreated;
+use Illuminate\Support\Facades\Event;
+use App\Http\Resources\CommentResource;
+use Illuminate\Support\Facades\Broadcast;
 use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
 
 class CreateCommentsTest extends TestCase
 {
@@ -42,20 +48,47 @@ class CreateCommentsTest extends TestCase
             'body' => $comment['body']
         ]);
     }
-        /** @test */
-        public function a_comment_requires_a_body()
-        {
-            $status = factory(Status::class)->create();
-            $user = factory(User::class)->create();
-            $this->actingAs($user);
 
-            $response = $this->postJson(route('statuses.comments.store', $status), ['body' => '']);
+    /** @test */
+    public function an_event_is_fired_when_a_comment_is_created()
+    {
+        Event::fake([CommentCreated::class]);
+        Broadcast::shouldReceive('socket')->andReturn('socket-id');
 
-            $response->assertStatus(422);
+        $status = factory(Status::class)->create();
+        $user = factory(User::class)->create();
+        $comment = ['body' => 'Mi primer comentario'];
 
-            $response->assertJsonStructure([
-                'message', 'errors' => ['body']
-            ]);
-        }
+        $this->actingAs($user)
+            ->postJson(route('statuses.comments.store', $status), $comment);
 
+        Event::assertDispatched(CommentCreated::class, function ($commentStatusEvent) {
+            $this->assertInstanceOf(ShouldBroadcast::class, $commentStatusEvent);
+            $this->assertInstanceOf(CommentResource::class, $commentStatusEvent->comment);
+            $this->assertInstanceOf(Comment::class, $commentStatusEvent->comment->resource);
+            $this->assertEquals(Comment::first()->id, $commentStatusEvent->comment->id);
+            $this->assertEquals(
+                'socket-id',
+                $commentStatusEvent->socket,
+                'The event ' . get_class($commentStatusEvent) . ' must call the method "dontBroadcastToCurrentUser" in the constructor.'
+            );
+            return true;
+        });
+    }
+
+    /** @test */
+    public function a_comment_requires_a_body()
+    {
+        $status = factory(Status::class)->create();
+        $user = factory(User::class)->create();
+        $this->actingAs($user);
+
+        $response = $this->postJson(route('statuses.comments.store', $status), ['body' => '']);
+
+        $response->assertStatus(422);
+
+        $response->assertJsonStructure([
+            'message', 'errors' => ['body']
+        ]);
+    }
 }

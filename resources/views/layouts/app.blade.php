f025bd6a5d46228b9853eeb2bdaa28fa06242e6a
cesar clemente fernandez dorantes <c.clemente1@hotmail.com>
2019-05-18 21:53:50 -0500

    Marcar notificaciones

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -9,7 +9,7 @@
     <meta name="user" content="{{ Auth::user() }}">
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.13/css/all.css" integrity="sha384-DNOHZ68U8hZfKXOrtjWvjxusGo9WQnrNx2sqG0tfsghAvtVlRW3tvkXWZh58N9jp" crossorigin="anonymous">
     <link rel="stylesheet" href="{{ mix('css/app.css') }}">
-    <title>SocialApp</title>
+    <title>Feibook</title>
 </head>
 <body>
 

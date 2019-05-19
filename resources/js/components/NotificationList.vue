37e704896e1c117d68aafaf40d58bf4103a3b9e9
cesar clemente fernandez dorantes <c.clemente1@hotmail.com>
2019-05-18 22:07:54 -0500

    Marcar notificaciones

--- resources/js/components/NotificationList.vue
+++ resources/js/components/NotificationList.vue
@@ -3,14 +3,16 @@
         <a href="#"
            dusk="notifications"
            class="nav-link dropdown-toggle"
+           :class="count ? 'text-primary font-weight-bold' : ''"
            id="dropdownNotifications"
            role="button"
            data-toggle="dropdown"
            aria-haspopup="true"
            aria-expanded="false">
-            <slot></slot>
+            <slot></slot> {{ count }}
         </a>
         <div class="dropdown-menu dropdown-menu-right" aria-labelledby="dropdownNotifications">
+            <div class="dropdown-header text-center">Notificaciones</div>
             <notification-list-item
                 v-for="notification in notifications"
                 :notification="notification"
@@ -27,14 +29,35 @@
         components: { NotificationListItem },
         data() {
             return {
-                notifications: []
+                notifications: [],
+                count: ''
             }
         },
         created() {
             axios.get('/notifications')
                 .then(res => {
                     this.notifications = res.data;
-                })
+                    this.unreadNotifications();
+                });
+
+            EventBus.$on('notification-was-read', () => {
+                if (this.count === 1)
+                {
+                    return this.count = '';
+                }
+                this.count--;
+            });
+
+            EventBus.$on('notification-was-unread', () => {
+                this.count++;
+            });
+        },
+        methods: {
+            unreadNotifications() {
+                this.count = this.notifications.filter(notification => {
+                    return notification.read_at === null;
+                }).length || ''
+            }
         }
     }
 </script>

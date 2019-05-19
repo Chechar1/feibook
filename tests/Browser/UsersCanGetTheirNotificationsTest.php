bb021415444c5fdcd18aff99e6aaebacfb95a94d
cesar clemente fernandez dorantes <c.clemente1@hotmail.com>
2019-05-19 00:39:04 -0500

    Notificaciones en tiempo real

--- tests/Browser/UsersCanGetTheirNotificationsTest.php
+++ tests/Browser/UsersCanGetTheirNotificationsTest.php
@@ -12,6 +12,7 @@ use Illuminate\Foundation\Testing\DatabaseMigrations;
 class UsersCanGetTheirNotificationsTest extends DuskTestCase
 {
     use DatabaseMigrations;
+
     /**
      * @test
      * @throws \Throwable
@@ -47,6 +48,32 @@ class UsersCanGetTheirNotificationsTest extends DuskTestCase
                 ->waitFor("@mark-as-read-{$notification->id}")
                 ->assertMissing("@mark-as-unread-{$notification->id}")
             ;
+        });
+    }
+
+    /**
+     * @test
+     * @throws \Throwable
+     */
+    public function users_can_see_their_notifications_in_real_time()
+    {
+        $user1 = factory(User::class)->create();
+        $user2 = factory(User::class)->create();
+
+        $status = factory(Status::class)->create(['user_id' => $user1->id]);
+
+        $this->browse(function (Browser $browser1, Browser $browser2) use ($user1, $user2, $status) {
+            $browser1->loginAs($user1)
+                ->visit('/')
+                ->resize(1024, 768)
+            ;
+
+            $browser2->loginAs($user2)
+                ->visit('/')
+                ->press('@like-btn')
+                ->pause(1000)
+            ;
+            $browser1->assertSeeIn('@notifications-count', 1);
         });
     }
 }

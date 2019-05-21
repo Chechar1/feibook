46c5276b7db2e03e62551a8bb7099421519c9292
cesar clemente fernandez dorantes <c.clemente1@hotmail.com>
2019-05-21 13:15:02 -0500

    Notificaciones en tiempo real

--- resources/js/components/NotificationList.vue
+++ resources/js/components/NotificationList.vue
@@ -34,6 +34,25 @@
             }
         },
         created() {
+
+            // Channel: private-App.User.3
+            // Event: Illuminate\Notifications\Events\BroadcastNotificationCreated
+
+            if (this.isAuthenticated)
+            {
+                Echo.private(`App.User.${this.currentUser.id}`)
+                    .notification(notification => {
+                        this.count++;
+                        this.notifications.push({
+                            id: notification.id,
+                            data: {
+                                link: notification.link,
+                                message: notification.message,
+                            }
+                        })
+                    })
+            }
+
             axios.get('/notifications')
                 .then(res => {
                     this.notifications = res.data;

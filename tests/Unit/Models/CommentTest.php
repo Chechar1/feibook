acde32d3b8d0d1bd7652a61c16f294a14b136940
cesar clemente fernandez dorantes <c.clemente1@hotmail.com>
2019-05-22 08:59:21 -0500

    Scroll a los comentarios

--- tests/Unit/Models/CommentTest.php
+++ tests/Unit/Models/CommentTest.php
@@ -8,11 +8,29 @@ use App\Models\Comment;
 use App\Traits\HasLikes;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 
-class CommentTest extends TestCase{
+class CommentTest extends TestCase
+{
+    use RefreshDatabase;
+
+    /** @test */
+    public function a_comment_belongs_to_a_user()
+    {
+        $comment = factory(Comment::class)->create();
+
+        $this->assertInstanceOf(User::class, $comment->user);
+    }
 
     /** @test */
     public function a_comment_model_must_use_the_trait_has_likes()
     {
         $this->assertClassUsesTrait(HasLikes::class, Comment::class);
+    }
+
+    /** @test */
+    public function a_comment_must_have_a_path()
+    {
+        $comment = factory(Comment::class)->create();
+
+        $this->assertEquals(route('statuses.show', $comment->status_id) . '#comment-' . $comment->id, $comment->path());
     }
 }

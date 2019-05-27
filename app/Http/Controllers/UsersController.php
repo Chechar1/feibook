eb743d765d3f7fe6acabac8b9f6d35c259f8c7d3
cesar clemente fernandez dorantes <c.clemente1@hotmail.com>
2019-05-27 09:44:49 -0500

    Cambios

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -17,28 +17,6 @@ class UsersController extends Controller
         return view('users.show', compact('user', 'friendshipStatus'));
     }
     public function store(){
-        // ruta de las imagenes guardadas
-        $ruta = public_path().'/avatar/';
-
-        // recogida del form
-        $imagenOriginal = $request->file('avatar');
-
-        // crear instancia de imagen
-        $imagen = Image::make($imagenOriginal);
-
-        // generar un nombre aleatorio para la imagen
-        $temp_name = $this->random_string() . '.' . $imagenOriginal->getClientOriginalExtension();
-
-        $imagen->resize(300,300);
-
-        // guardar imagen
-        // save( [ruta], [calidad])
-        $imagen->save($ruta . $temp_name, 100);
-
-
-        $personaje = new Personaje;
-        $personaje->filename = $temp_name;
-        $personaje->save();
         return view('users.avatar', compact('avatar'));
     }
 }
